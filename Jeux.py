2eeb0fe3975d5f4a5a55369e7dad8f3369e9b4f7
Akerman64 <ndiassembeguere@gmail.com>
2022-11-13 04:16:39 +0000

    second commit

--- Jeux.py
+++
@@ -1,9 +0,0 @@
-import random
-
-mots = []
-with open ("mots.txt") as fl:
-            for l in fl:
-                mots.append(l.rstrip("\n"))
-while len(mots) == 4 :                
-    mot = random.choice(mots)
-print(mot)
